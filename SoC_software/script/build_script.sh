66160de1dbb2801d62dc1a52950441da44d2b26b
alexandresoulier <40261492+alexandresoulier@users.noreply.github.com>
2018-06-14 10:40:34 +0200

    Add files via upload

---
+++ SoC_software/script/build_script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+
+arm-linux-gnueabihf-gcc \
+-g \
+-O0 \
+-Wall \
+-Werror \
+-I${SOCEDS_DEST_ROOT}/ip/altera/hps/altera_hps/hwlib/include \
+-I${SOCEDS_DEST_ROOT}/ip/altera/hps/altera_hps/hwlib/include/soc_cv_av \
+-D soc_cv_av \
+../src/main.c \
+-o ../tcp_test
